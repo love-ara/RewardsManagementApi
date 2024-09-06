5ce220b8430bd81b6cab7fece45dcb82ace53206
Aramide <aramideotenaike@gmail.com>
2024-09-06 20:39:59 +0100

    initial commit

---
+++ rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/RewardsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.balancee.rewardsManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsManagementApiApplication.class, args);
+	}
+
+}
---
+++ rewardsManagementApi/src/test/java/com/balancee/rewardsManagementApi/RewardsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balancee.rewardsManagementApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
