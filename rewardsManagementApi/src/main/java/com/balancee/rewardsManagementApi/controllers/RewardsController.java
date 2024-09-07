7f91e333fad8a4db89649f8c077db88c335b45d3
Aramide <aramideotenaike@gmail.com>
2024-09-07 18:54:30 +0100

    refactored code

--- rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/controllers/RewardsController.java
+++ rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/controllers/RewardsController.java
@@ -2,6 +2,7 @@ package com.balancee.rewardsManagementApi.controllers;
 
 import com.balancee.rewardsManagementApi.dtos.requests.GetCashbackHistoryRequest;
 import com.balancee.rewardsManagementApi.dtos.requests.GetRewardsBalanceRequest;
+import com.balancee.rewardsManagementApi.dtos.responses.BaseResponse;
 import com.balancee.rewardsManagementApi.exceptions.CustomerNotFoundException;
 import com.balancee.rewardsManagementApi.services.RewardsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/rewards")
@@ -24,12 +27,14 @@ public class RewardsController {
 
     @GetMapping("/balance")
     public ResponseEntity<?> getRewardsBalance(@RequestBody GetRewardsBalanceRequest request) throws CustomerNotFoundException {
-       return ResponseEntity.ok(rewardsService.getRewardsBalance(request));
+       return new ResponseEntity<>(new BaseResponse<>(200,true,
+               rewardsService.getRewardsBalance(request)), OK);
     }
 
     @GetMapping("/history")
     public ResponseEntity<?> getCashbackHistory(@RequestBody GetCashbackHistoryRequest request) throws CustomerNotFoundException{
-        return ResponseEntity.ok(rewardsService.getCashbackHistory(request));
+        return new ResponseEntity<>(new BaseResponse<>(200, true,
+                rewardsService.getCashbackHistory(request)), OK);
     }
 
 }
--- rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/dtos/responses/BaseResponse.java
+++ rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/dtos/responses/BaseResponse.java
@@ -1,10 +1,12 @@
 package com.balancee.rewardsManagementApi.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BaseResponse <T> {
     private int code;
     private boolean status;
