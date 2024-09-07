a2c0b02f7ccd4119df60efba8fc99bdd1cc961b7
Aramide <aramideotenaike@gmail.com>
2024-09-07 19:09:30 +0100

    refactored code

--- rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/dtos/responses/BaseResponse.java
+++ rewardsManagementApi/src/main/java/com/balancee/rewardsManagementApi/dtos/responses/BaseResponse.java
@@ -2,11 +2,13 @@ package com.balancee.rewardsManagementApi.dtos.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BaseResponse <T> {
     private int code;
     private boolean status;
